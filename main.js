147cd52c391e09f52dae41ad62f81153b1ead589
Graf-Lev-V <legendarlev2004@gmail.com>
2026-03-20 19:08:48 +0300

    1

--- main.js
+++ main.js
@@ -1,75 +1,77 @@
-const viewport = document.querySelector("[data-reviews-viewport]");
-const track = document.querySelector("[data-reviews-track]");
-const btnPrev = document.querySelector("[data-reviews-prev]");
-const btnNext = document.querySelector("[data-reviews-next]");
+const viewport = document.querySelector('[data-reviews-viewport]');
+const track = document.querySelector('[data-reviews-track]');
+const btnPrev = document.querySelector('[data-reviews-prev]');
+const btnNext = document.querySelector('[data-reviews-next]');
 const slides = Array.from(track.children);
 
 let prevActiveIndex = 0;
 let activeIndex = 0;
 
-window.addEventListener("resize", updateUI)
+window.addEventListener('resize', updateUI);
 
 updateUI();
 
 function updateUI() {
-    const maxScroll = (slides.length - 1) * (slides[0].getBoundingClientRect().width + parseFloat(getComputedStyle(track).gap));
+  const maxScroll =
+    (slides.length - 1) *
+    (slides[0].getBoundingClientRect().width +
+      parseFloat(getComputedStyle(track).gap));
 
-    btnPrev.disabled = viewport.scrollLeft <= 0;
-    btnNext.disabled = viewport.scrollLeft >= maxScroll - 1;
-    
-    const noActiveUpdate = slides[prevActiveIndex];
-    const activeUpdate = slides[activeIndex];
+  btnPrev.disabled = viewport.scrollLeft <= 0;
+  btnNext.disabled = viewport.scrollLeft >= maxScroll - 1;
 
-    noActiveUpdate.classList.remove("is-active");
+  const noActiveUpdate = slides[prevActiveIndex];
+  const activeUpdate = slides[activeIndex];
 
-    activeUpdate.classList.add("is-active");
+  noActiveUpdate.classList.remove('is-active');
 
-    syncArrowIcon(btnPrev);
-    syncArrowIcon(btnNext);
+  activeUpdate.classList.add('is-active');
+
+  syncArrowIcon(btnPrev);
+  syncArrowIcon(btnNext);
 }
 
 function syncArrowIcon(button) {
-    const img = button.querySelector("img");
+  const img = button.querySelector('img');
 
-    const enabledSrc = img.dataset.srcEnabled;
-    const disabledSrc = img.dataset.srcDisabled;
+  const enabledSrc = img.dataset.srcEnabled;
+  const disabledSrc = img.dataset.srcDisabled;
 
-    img.src = button.disabled ? disabledSrc : enabledSrc;
+  img.src = button.disabled ? disabledSrc : enabledSrc;
 }
 
 let isAnimating = false;
 
-btnNext.addEventListener("click", scrollNext);
-btnPrev.addEventListener("click", scrollPrev);
-viewport.addEventListener("scrollend", () => {
-    updateUI();
-    isAnimating = false;
+btnNext.addEventListener('click', scrollNext);
+btnPrev.addEventListener('click', scrollPrev);
+viewport.addEventListener('scrollend', () => {
+  updateUI();
+  isAnimating = false;
 });
 
 function getScrollLength() {
-    return slides[0].getBoundingClientRect().width + parseFloat(getComputedStyle(track).gap);
+  return (
+    slides[0].getBoundingClientRect().width +
+    parseFloat(getComputedStyle(track).gap)
+  );
 }
 
 function scrollNext() {
+  if (isAnimating) return;
+  isAnimating = true;
 
-    if (isAnimating) return;
-    isAnimating = true;
-    
-    viewport.scrollBy({left: getScrollLength(), behavior: "smooth"});
+  viewport.scrollBy({ left: getScrollLength(), behavior: 'smooth' });
 
-    
-    prevActiveIndex = activeIndex;
-    activeIndex += 1;
+  prevActiveIndex = activeIndex;
+  activeIndex += 1;
 }
 
 function scrollPrev() {
+  if (isAnimating) return;
+  isAnimating = true;
 
-    if (isAnimating) return;
-    isAnimating = true;
+  viewport.scrollBy({ left: -getScrollLength(), behavior: 'smooth' });
 
-    viewport.scrollBy({left: -getScrollLength(), behavior: "smooth"});
-
-
-    prevActiveIndex = activeIndex;
-    activeIndex -= 1;
+  prevActiveIndex = activeIndex;
+  activeIndex -= 1;
 }
